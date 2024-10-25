7500ad3f42b32dd7a98c79858710dfe8e16458ed
Hariom Rajak <hariomrajak463@gmail.com>
2024-10-25 21:15:55 +0530

    initialize commit

---
+++ app/src/main/java/com/example/womensafetyapp/MapsActivity.kt
@@ -0,0 +1,74 @@
+package com.example.womensafetyapp.ui
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.womensafetyapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        // Initialize location provider
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Initialize map fragment
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Check location permissions
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            return
+        }
+
+        // Enable user's current location
+        mMap.isMyLocationEnabled = true
+
+        // Get current location
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                val gettingid :String = intent.getStringExtra("extra").toString()
+                val gmmIntentUri = Uri.parse("geo:${location.latitude},${location.longitude}?q=$gettingid")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+            }
+        }
+    }
+
+    // Handle permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            onMapReady(mMap)
+        } else {
+            Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/womensafetyapp/splash_screen.kt
@@ -0,0 +1,33 @@
+package com.example.womensafetyapp
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        val womentv = findViewById<TextView>(R.id.womentv)
+        val safetytv = findViewById<TextView>(R.id.safetytv)
+        // Create an ObjectAnimator to slide the logo down
+        ObjectAnimator.ofFloat(womentv, "translationY", -300f,0f).apply {
+            duration = 1500 // Set the duration of the animation
+            start() // Start the animation
+        }
+        ObjectAnimator.ofFloat(safetytv, "translationY", 300f,0f).apply {
+            duration = 1500 // Set the duration of the animation
+            start()
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, signup_page::class.java))
+            finish() // Close the splash screen activity
+        }, 2000)
+    }
+}
